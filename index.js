51b56ef53122136fcd0e717659d59e21ef03d985
proteinanalyser <41210813+proteinanalyser@users.noreply.github.com>
2018-07-19 11:26:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var multer  = require('multer')
+var multer = require('multer')
 var XLSX = require('xlsx')
-
+var fs = require("fs")
 const storage = multer.diskStorage({
   destination: "./uploads",
   filename: function (req, file, cb) {
@@ -22,14 +22,10 @@ function checkFileType(file, cb) {
   let filetypes = ".xlsx";
   let extname = path.extname(file.originalname).toLowerCase() == filetypes;
   let mimetype = file.mimetype == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  if (originalname != ".xlsx") {
+  if (path.extname(file.originalname) != ".xlsx") {
     cb("The file you uploaded has a wrong file extension! The file must be .xlsx");
-  } else if(file.name !="template.xlsx"){
+  } else if (file.originalname != "template.xlsx") {
     cb("Only upload the template file provided by this platform!");
-  } else if ( XLSX.read("./uploads/template.xlsx", {type:'file'}).SheetNames.length > 1){
-    cb("Please do not add any excel sheets to the template.");
-  }else if ( XLSX.read("./uploads/template.xlsx", {type:'file'}).SheetNames[0] != "Sheet1"){
-    cb("Please do not change the name of the excel sheet");
   } else if (mimetype && extname) {
     return cb(null, true);
   }
@@ -64,7 +60,8 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.post("/parse_excel", function(req, res){
+app.post("/parse_excel", function (req, res) {
+
   upload(req, res, (err) => {
     if (err) {
       res.send({
@@ -72,18 +69,52 @@ app.post("/parse_excel", function(req, res){
         msg: err
       });
     } else {
-      var workbook = XLSX.read("./uploads/template.xlsx", {type:'file'});
+      if (XLSX.read("./uploads/template.xlsx", {
+          type: 'file'
+        }).SheetNames.length > 1) {
+        res.send({
+          error: true,
+          msg: "Please do not add any excel sheets to the template."
+        });
+      } else if (XLSX.read("./uploads/template.xlsx", {
+          type: 'file'
+        }).SheetNames[0] != "Sheet1") {
+        res.send({
+          error: true,
+          msg: "Please do not change the name of the excel sheet"
+        });
+      }
+      var workbook = XLSX.read("./uploads/template.xlsx", {
+        type: 'file'
+      });
       var first_sheet_name = workbook.SheetNames[0];
       var worksheet = workbook.Sheets[first_sheet_name];
-      
-      res.send({
-        error: false,
-        data: XLSX.utils.sheet_to_json(worksheet)
+      var sendJSON = XLSX.utils.sheet_to_json(worksheet, {
+        header: "A"
+      })
+      var keys = Object.keys(sendJSON[0]);
+      var sample_names = [];
+      var input_atttributes = {};
+      for (var i = 0; i < keys.length; i++) {
+        sample_names.push(sendJSON[0][keys[i]]);
+        input_atttributes[sendJSON[0][keys[i]]] = [];
+        for (var k = 1; k < sendJSON.length; k++) {
+          input_atttributes[sendJSON[0][keys[i]]].push(sendJSON[k][keys[i]])
+        }
+      }
+      fs.unlink("./uploads/template.xlsx", () => {
+        res.send({
+          error: false,
+          data: {
+            "sample_names": sample_names,
+            "input_atttributes": input_atttributes
+          }
+        })
       })
     }
   });
-})
-// 
-app.listen(app.get("port"), function() {
+});
+
+app.listen(app.get("port"), function () {
   console.log("App is listenning o port 3000");
 });
